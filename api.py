7c5bae5d842629185da1923974ef1b2aaa6c0f1f
Ras Soyungalgo <agalvezcorell@gmail.com>
2020-04-11 20:22:47 +0200

    Monto API con streamlit

--- api.py
+++ api.py
@@ -1,34 +1,33 @@
-import src.getinfo as get
-from flask import Flask, request
-import os
-import json
+import src.cleandata as clean
+import streamlit as st
+import pandas as pd
 
-app = Flask(__name__)
+st.title("Who eats the food we grow?")
+st.write(
+"""
+In this visualization project, I have created an API that allows us to see the data of the production of different foods in the countries and if they are used for food or feed. I have extracted the data from [this Kaggle dataset.](https://www.kaggle.com/dorbicycle/world-foodfeed-production)
+"""
+)
 
+@st.cache(persist=True)
+def load_data():
+    data = clean.loadCleanData()
+    return data
+#load all data
+data = load_data()
 
-@app.route('/<country>/<year>/<product>')
-def allInfo(country,year,product):
-    info = get.givenYear(country,year,product)
-    return json.dumps(info)
+#select box for choose country
+area = "Area"
+option = st.selectbox(
+    'Choose an area to visualize data',
+     clean.choice(area))
+'You selected: ', option
 
+#select box for choose item
+item = "Item"
+option2 = st.selectbox(
+    'Select the product to see the production evolution',
+    clean.choice(item)
+)
+'You selected: ', option2
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.run("0.0.0.0", "5000", debug=True)
--- src/cleandata.py
+++ src/cleandata.py
@@ -1,19 +1,29 @@
 import pandas as pd
-import json
 
-#reading data
-df = pd.read_csv("../input/FAO.csv",encoding='cp1252')
+def loadCleanData():
+    """
+    Load data, rename year`s columns, drop duplicates
+    and return clean dataframe
+    """
+    df = pd.read_csv("input/FAO.csv",encoding='cp1252')
+    columnas = df.columns[10:]
+    nombres = []
+    for a in columnas:
+        nombres.append((a,a[1:]))
+    nombres = dict(nombres)
+    df = df.rename(columns=nombres)
+    df = df.drop_duplicates()
+    return df
 
-#rename year`s columns 
-columnas = df.columns[10:]
-nombres = []
-for a in columnas:
-    nombres.append((a,a[1:]))
-nombres = dict(nombres)
-df = df.rename(columns=nombres)
 
-#drop duplicates
-df = df.drop_duplicates()
+def choice(param):
+    data = loadCleanData()
+    dfarea = {"Area":data.Area.unique()}
+    data1 = pd.DataFrame(dfarea)
+    dfitem = {"Item":data.Item.unique()}
+    data2 = pd.DataFrame(dfitem)
 
-#export json to MongoDB
-df.to_json("../input/data", orient="records")
+    if param == "Item":
+        return data2["Item"]
+    else:
+        return data1["Area"]
