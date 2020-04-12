843ff4a41916b23afcf68bfb1abb03122e0ceea9
Ras Soyungalgo <agalvezcorell@gmail.com>
2020-04-12 19:15:14 +0200

    foto api

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 import src.cleandata as clean
 import streamlit as st
-import pandas as pd
+from PIL import Image
 
-st.title("Who eats the food we grow?")
+st.markdown("<h1 style='text-align: center; color: black;'>Who eats the food we grow?</h1>", unsafe_allow_html=True)
+
+image = Image.open('input/portadapi.jpg')
+st.image (image,use_column_width=True)
 st.write(
 """
 In this visualization project, I have created an API that allows us to see the data of the production of different foods in the countries and if they are used for food or feed. I have extracted the data from [this Kaggle dataset.](https://www.kaggle.com/dorbicycle/world-foodfeed-production)
